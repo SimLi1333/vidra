d820e3a1dfacbabb40a95fe47f04c688ddd85584
simon.linder <simon.linder@ost.ch>
2025-05-29 18:17:48 +0200

    adding tests

--- internal/adapter/k8s/watch_resources_test.go
+++ internal/adapter/k8s/watch_resources_test.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic/fake"
 	k8stesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/cache"
 )
 
 type callbackMock struct {
@@ -22,27 +23,20 @@ func (m *callbackMock) Callback(obj *unstructured.Unstructured, gvr schema.Group
 }
 
 func TestStartWatchingGVRs_AddEvent(t *testing.T) {
-	// Setup
 	scheme := runtime.NewScheme()
 	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
 
-	// Register GVR to ListKind mapping to prevent panic
-	client := fake.NewSimpleDynamicClientWithCustomListKinds(
-		scheme,
-		map[schema.GroupVersionResource]string{
-			gvr: "FooList",
-		},
-	)
+	client := fake.NewSimpleDynamicClientWithCustomListKinds(scheme, map[schema.GroupVersionResource]string{
+		gvr: "FooList",
+	})
 
 	obj := &unstructured.Unstructured{}
 	obj.SetGroupVersionKind(gvr.GroupVersion().WithKind("Foo"))
 	obj.SetName("foo1")
 	obj.SetLabels(map[string]string{"managed-by": "vida"})
 
-	// Prepare the fake Watch interface
 	watcher := watch.NewFake()
-
-	client.PrependWatchReactor("foos", func(action k8stesting.Action) (handled bool, ret watch.Interface, err error) {
+	client.PrependWatchReactor("foos", func(action k8stesting.Action) (bool, watch.Interface, error) {
 		return true, watcher, nil
 	})
 
@@ -55,60 +49,123 @@ func TestStartWatchingGVRs_AddEvent(t *testing.T) {
 	})
 
 	cb := new(callbackMock)
-	cb.On("Callback", mock.AnythingOfType("*unstructured.Unstructured"), gvr).Once()
+	done := make(chan struct{})
+	cb.On("Callback", mock.AnythingOfType("*unstructured.Unstructured"), gvr).Run(func(args mock.Arguments) {
+		close(done)
+	}).Once()
 
 	factory := NewDynamicWatcherFactory()
-
-	// Act
 	go factory.StartWatchingGVRs(client, []schema.GroupVersionResource{gvr}, cb.Callback)
 
-	// Trigger the event
-	time.Sleep(100 * time.Millisecond) // Wait for informer to start
+	time.Sleep(100 * time.Millisecond)
 	watcher.Add(obj)
 
-	// Assert
-	time.Sleep(100 * time.Millisecond)
-	cb.AssertExpectations(t)
+	select {
+	case <-done:
+		cb.AssertExpectations(t)
+	case <-time.After(1 * time.Second):
+		t.Fatal("Expected callback not called")
+	}
 }
 
-func TestStartWatchingGVRs_SkipAlreadyStarted(t *testing.T) {
-	scheme := runtime.NewScheme()
-	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
-
-	client := fake.NewSimpleDynamicClientWithCustomListKinds(
-		scheme,
-		map[schema.GroupVersionResource]string{
-			gvr: "FooList",
-		},
-	)
-
+func TestAddFunc_WithTombstone_Unstructured(t *testing.T) {
 	cb := new(callbackMock)
-	cb.On("Callback", mock.AnythingOfType("*unstructured.Unstructured"), gvr).Once()
 
-	factory := NewDynamicWatcherFactory()
-
-	// Start watching the same GVR twice
-	go factory.StartWatchingGVRs(client, []schema.GroupVersionResource{gvr}, cb.Callback)
-	go factory.StartWatchingGVRs(client, []schema.GroupVersionResource{gvr}, cb.Callback)
-
-	// Trigger a watch event
-	watcher := watch.NewFake()
-	client.PrependWatchReactor("foos", func(action k8stesting.Action) (bool, watch.Interface, error) {
-		return true, watcher, nil
-	})
-
-	// Wait for goroutines to run
-	time.Sleep(100 * time.Millisecond)
+	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
 
 	obj := &unstructured.Unstructured{}
 	obj.SetGroupVersionKind(gvr.GroupVersion().WithKind("Foo"))
-	obj.SetName("test-foo")
+	obj.SetName("tombstoned-foo")
 	obj.SetLabels(map[string]string{"managed-by": "vida"})
 
-	watcher.Add(obj)
+	tombstone := cache.DeletedFinalStateUnknown{
+		Key: "test/tombstoned-foo",
+		Obj: obj,
+	}
 
-	time.Sleep(100 * time.Millisecond)
+	cb.On("Callback", obj, gvr).Once()
 
-	// Expect callback to have been called once
+	handler := getEventHandler(gvr, cb.Callback)
+	handler.AddFunc(tombstone)
+
+	cb.AssertExpectations(t)
+}
+
+func TestUpdateFunc_GenerationChange(t *testing.T) {
+	cb := new(callbackMock)
+
+	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
+
+	oldObj := &unstructured.Unstructured{}
+	oldObj.SetGeneration(1)
+	oldObj.SetName("gen-change-foo")
+	oldObj.SetLabels(map[string]string{"managed-by": "vida"})
+
+	newObj := &unstructured.Unstructured{}
+	newObj.SetGeneration(2)
+	newObj.SetName("gen-change-foo")
+	newObj.SetLabels(map[string]string{"managed-by": "vida"})
+
+	cb.On("Callback", newObj, gvr).Once()
+
+	handler := getEventHandler(gvr, cb.Callback)
+	handler.UpdateFunc(oldObj, newObj)
+
+	cb.AssertExpectations(t)
+}
+
+func TestUpdateFunc_NoGenerationChange(t *testing.T) {
+	cb := new(callbackMock)
+
+	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
+
+	obj := &unstructured.Unstructured{}
+	obj.SetGeneration(1)
+	obj.SetName("same-gen-foo")
+	obj.SetLabels(map[string]string{"managed-by": "vida"})
+
+	// No expected call since generation didn't change
+	handler := getEventHandler(gvr, cb.Callback)
+	handler.UpdateFunc(obj, obj)
+
+	cb.AssertExpectations(t) // Ensures no unexpected calls were made
+}
+
+func TestDeleteFunc_WithUnstructured(t *testing.T) {
+	cb := new(callbackMock)
+
+	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
+
+	deleted := &unstructured.Unstructured{}
+	deleted.SetName("my-resource")
+	deleted.SetLabels(map[string]string{"managed-by": "vida"}) // <-- add this label
+
+	cb.On("Callback", deleted, gvr).Once()
+
+	handler := getEventHandler(gvr, cb.Callback)
+	handler.DeleteFunc(deleted)
+
+	cb.AssertExpectations(t)
+}
+
+func TestDeleteFunc_WithTombstone(t *testing.T) {
+	cb := new(callbackMock)
+
+	gvr := schema.GroupVersionResource{Group: "test", Version: "v1", Resource: "foos"}
+
+	obj := &unstructured.Unstructured{}
+	obj.SetName("tombstoned-foo")
+	obj.SetLabels(map[string]string{"managed-by": "vida"})
+
+	tombstone := cache.DeletedFinalStateUnknown{
+		Key: "test/tombstoned-foo",
+		Obj: obj,
+	}
+
+	cb.On("Callback", obj, gvr).Once()
+
+	handler := getEventHandler(gvr, cb.Callback)
+	handler.DeleteFunc(tombstone)
+
 	cb.AssertExpectations(t)
 }
